56af4a57cb11491251d576097cd67fd9f3afe486
Alessia Pileri <alessiapileri@Alessias-MacBook-Pro.local>
2017-12-06 11:20:06 +0100

    chore: Change log level for device values handler

--- src/events/handlers/device/common/values/deviceValuesHandler.js
+++ src/events/handlers/device/common/values/deviceValuesHandler.js
@@ -2,7 +2,7 @@ import logger from '../../../../../common/logger';
 
 const DeviceValuesHandler = deviceValuesProvider => ({
   process: ({ timestamp, gateway, deviceId, type, value, unit }) => {
-    logger.log('info', `device values processor ${JSON.stringify({ deviceId, type, value, unit })}`);
+    logger.log('debug', `device values processor ${JSON.stringify({ deviceId, type, value, unit })}`);
 
     return new Promise((resolve, reject) => {
       deviceValuesProvider
