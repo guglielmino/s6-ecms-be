f3178feea090a5765fff9f8359a091218fc76d8c
Alessia Pileri <alessiapileri@Alessias-MBP.homenet.telecomitalia.it>
2018-01-31 18:05:34 +0100

    fix: Hourly stats report with device description instead of device name

--- src/api/controllers/stats/hourly/hourlyStatTransformer.js
+++ src/api/controllers/stats/hourly/hourlyStatTransformer.js
@@ -1,6 +1,6 @@
 const transformHourlyStat = stat => ({
   deviceId: stat.deviceId || stat._id.deviceId ? stat.deviceId || stat._id.deviceId : '', // eslint-disable-line no-underscore-dangle
-  deviceName: stat.device && stat.device[0] ? stat.device[0].name : '',
+  deviceName: stat.device && stat.device[0] ? stat.device[0].description : '',
   power: stat.power,
   date: stat.date || stat._id.date, // eslint-disable-line no-underscore-dangle
 });
