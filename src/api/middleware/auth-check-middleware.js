6bb0e8eb79999a38af434d47216bab7a1aaf2d96
Fabrizio Guglielmino <guglielmino@gmail.com>
2018-09-07 23:00:45 +0200

    fix: Fix auth middleware

--- src/api/middleware/auth-check-middleware.js
+++ src/api/middleware/auth-check-middleware.js
@@ -1,15 +1,18 @@
 import jwt from 'express-jwt';
 import jwksRsa from 'jwks-rsa';
 
-export default ({ clientID, domain }) => () =>
-  jwt({
-    secret: jwksRsa.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri: `https://${domain}/.well-known/jwks.json`,
-    }),
+export default ({ clientID, domain }) => () => {
+  const sec = jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${domain}/.well-known/jwks.json`,
+  });
+
+  return jwt({
+    secret: sec,
     audience: clientID,
     issuer: `https://${domain}/`,
     algorithms: ['RS256'],
   });
+};
